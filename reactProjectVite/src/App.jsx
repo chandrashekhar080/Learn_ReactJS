3640787fa7141fcc4295dc6de43357cee2622bb6
chandrashekhar <0810chandrashekhar@gmail.com>
2025-02-11 15:35:09 +0530

    Add reactProjectVite And reactProjectMyApp

---
+++ reactProjectVite/src/App.jsx
@@ -0,0 +1,15 @@
+import CustomComponent from "./CustomComponent";
+
+function App(){
+  return (
+    <div>
+      <h1>Hello World !</h1>
+      <CustomComponent />
+      <CustomComponent />
+      <CustomComponent />
+      <CustomComponent />
+      <CustomComponent />
+    </div>
+  )
+}
+ export default App;
---
+++ reactProjectVite/src/CustomComponent.jsx
@@ -0,0 +1,9 @@
+function CustomComponent() {
+    return (
+        <div>
+            Custom Component !
+        </div>
+    )
+}
+
+export default CustomComponent;
